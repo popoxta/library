0a2c0814ed47b2a1d280f7a9cfb04b31be19de28
popoxta <popoxta@gmail.com>
2023-03-16 13:27:37 +1300

    refactor scripts and minor css / html changes

--- script.js
+++ script.js
@@ -6,22 +6,17 @@ class Book {
         this.status = status
     }
 }
-
-let booksArray = JSON.parse(localStorage.getItem("booksArray"))
-if (booksArray === null){
-    booksArray = []
-
-    const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", "295", false)
-    booksArray.push(theHobbit)
-
-    const praiseOfFolly = new Book("In Praise of Folly", "Desiderius Erasmus", "256", false)
-    booksArray.push(praiseOfFolly)
-
-    const theArtOfWar = new Book("The Art of War", "Sun Tzu", "260", false)
-    booksArray.push(theArtOfWar)
-
-    const nineteenEightyFour = new Book("1984", "George Orwell", "328", false)
-    booksArray.push(nineteenEightyFour)
+function getLocalStorage() {
+    let books = JSON.parse(localStorage.getItem("booksArray"))
+    if (!books) {
+        books = []
+        const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", "295", false)
+        const praiseOfFolly = new Book("In Praise of Folly", "Desiderius Erasmus", "256", false)
+        const theArtOfWar = new Book("The Art of War", "Sun Tzu", "260", false)
+        const nineteenEightyFour = new Book("1984", "George Orwell", "328", false)
+        books.push(theHobbit, praiseOfFolly, theArtOfWar, nineteenEightyFour)
+    }
+    return books
 }
 
 function updateLocalStorage(){
@@ -34,99 +29,79 @@ function drawLibrary() {
     const books = document.querySelectorAll(".book")
     const library = document.querySelector(".library")
 
-    for (let book of books) {
-        library.removeChild(book)
-    }
-
+    for (let book of books) library.removeChild(book)
     for (let book of booksArray) {
-        console.log(book)
-
         const container = document.createElement("div")
         container.className = "book shadow"
-
         const deleteBtn = document.createElement("button")
         deleteBtn.className = "delete"
 
         deleteBtn.addEventListener("click", () => {
-
             if (confirm("Are you sure you would like to delete this book?")) {
-
                 booksArray.splice(booksArray.indexOf(book), 1)
-                console.log(`Deleted ${book.name}`)
                 drawLibrary()
             }
-
         })
 
         const title = document.createElement("h2")
         title.textContent = book.name
-
         const theHr = document.createElement("hr")
-
         const author = document.createElement("h3")
         author.textContent = `By ${book.author}`
-
         const pages = document.createElement("h3")
         pages.textContent = `${book.pages} pages`
-
         const readBtn = document.createElement("button")
 
         container.append(deleteBtn, title, theHr, author, pages, readBtn)
 
-        updateStatus(readBtn, book.status)
-        readBtn.addEventListener("click", (e) => {
+        function updateStatus(readBtn, val) {
+            readBtn.className = val ? "read" : "not-read"
+        }
 
+        updateStatus(readBtn, book.status)
+
+        readBtn.addEventListener("click", (e) => {
             book.status = !book.status
             updateStatus(e.target, book.status)
             updateLocalStorage()
-
         })
-
         library.append(container)
     }
-
 }
 
-function updateStatus(readBtn, val) {
-    readBtn.className = val ? "read" : "not-read"
-}
+function drawForms() {
+    const openAddBookForm = document.querySelector(".add-item")
+    const closeBookForm = document.querySelector(".close-form")
+    const addBookForm = document.querySelector(".book-form")
 
-const openAddBookForm = document.querySelector(".add-item")
-const closeBookForm = document.querySelector(".close-form")
-const addBookForm = document.querySelector(".book-form")
+    openAddBookForm.addEventListener("click", () => toggleShowForm())
+    closeBookForm.addEventListener("click", () => toggleShowForm())
 
-openAddBookForm.addEventListener("click", () => {
-
-    if (addBookForm.classList.contains("hidden")) {
-        addBookForm.classList.replace("hidden", "show")
-
-    } else {
-        addBookForm.classList.replace("show", "hidden")
+    function toggleShowForm() {
+        addBookForm.classList.toggle('hidden')
+        addBookForm.classList.toggle('show')
+        addBookForm.reset()
     }
-})
 
-closeBookForm.addEventListener("click", () => {
-    addBookForm.classList.replace("show", "hidden")
-    addBookForm.reset()
-})
+    addBookForm.addEventListener("submit", (e) => {
+        e.preventDefault()
+        const values = e.target
+        const formData = new FormData(values)
 
-addBookForm.addEventListener("submit", (e) => {
-    e.preventDefault()
-    const formData = new FormData(e.target)
+        const newBook = new Book(
+            formData.get("title"),
+            formData.get("author"),
+            formData.get("pages"),
+            formData.get("read") === "on",
+        )
+        booksArray.push(newBook)
 
-    const newBook = new Book(
-        formData.get("title"),
-        formData.get("author"),
-        formData.get("pages"),
-        formData.get("read") === "on",
-    )
-    booksArray.push(newBook)
+        addBookForm.classList.replace("show", "hidden")
+        values.reset()
 
-    addBookForm.classList.replace("show", "hidden")
-    e.target.reset()
-
-    drawLibrary()
-
-})
-
+        drawLibrary()
+    })
+}
+let booksArray = getLocalStorage()
+drawForms()
 drawLibrary()
